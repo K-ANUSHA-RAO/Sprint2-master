6b23937c2ba71cd8b65b70545735b90199ac7709
K-ANUSHA-RAO <anusharao4520@gmail.com>
2022-06-21 17:43:10 +0530

    Made the navbar into light color in dashboard

--- Sprint2-master/Expense/src/components/EmployeeDashboard/EmployeeDashboard.js
+++ Sprint2-master/Expense/src/components/EmployeeDashboard/EmployeeDashboard.js
@@ -10,12 +10,13 @@ class EmployeeDashboard extends React.Component {
     render(){
     return(
        
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+          <div className='container'>
             <div className="navbar-nav mr-auto">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="row col-12 d-flex collapse navbar-collapse" id="navbarNavDropdown">
+            <div className="row col-24 d-flex collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
             <Link to={"/Reimbursement Form"} className="nav-link dark">
@@ -23,11 +24,12 @@ class EmployeeDashboard extends React.Component {
               </Link>
             </li>
             <li className="nav-item active">
-              <Link to={"/ViewList"} className="nav-link">
+              <Link to={"/ViewList"} className="nav-link dark">
               View Reimbursement Status 
               </Link>
             </li>
             </ul>
+            </div>
             </div>
                 </div>
                 <div className="container mt-3">
--- Sprint2-master/Expense/src/components/ManagerDashboard/ManagerDashboard.js
+++ Sprint2-master/Expense/src/components/ManagerDashboard/ManagerDashboard.js
@@ -11,22 +11,27 @@ class ManagerDashboard extends React.Component {
     render(){
         return(
            
-            <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-sm navbar-light bg-light" >
+              <div className='container'>
                 <div className="navbar-nav mr-auto">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <li className="nav-item dark" >
+                <div className="row col-24 d-flex collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav mr-auto">
+                <li className="nav-item active" >
                 <Link to={"/ViewAllList"} className="nav-link dark">
                 View All forms
                   </Link>
                 </li>
-                <li className="nav-item ">
-                  <Link to={"/Accept And Reject Requests"} className="nav-link">
+                <li className="nav-item active">
+                  <Link to={"/Accept And Reject Requests"} className="nav-link dark">
                   Accept And Reject Requests
                   </Link>
                 </li>
-                
+                </ul>
+                </div>
+                </div>
                     </div>
                     <div className="container mt-3">
               <Switch>
